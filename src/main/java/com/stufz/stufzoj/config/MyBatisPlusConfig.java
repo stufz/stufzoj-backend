495946af90b0f3254e396d419141bbc9d08ed304
stufz <1063297049@qq.com>
2024-04-08 18:32:56 +0800

    题目相关表的创建、相关model、server的自动生成、以及相关controller的编写

--- src/main/java/com/stufz/stufzoj/config/MyBatisPlusConfig.java
+++ src/main/java/com/stufz/stufzoj/config/MyBatisPlusConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * MyBatis Plus 配置
  *
- * @author https://github.com/liyupi
+ *
  */
 @Configuration
 @MapperScan("com.stufz.stufzoj.mapper")
